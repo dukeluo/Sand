a8f59198b71c1a24adc18f435722d921beb2f09e
duke <dukeluo@outlook.com>
2019-07-22 12:38:41 +0800

    feat: render earthquake title

--- geoquakes/main.js
+++ geoquakes/main.js
@@ -1,8 +1,31 @@
 // wait for DOM to load before running JS
 $(function() {
 
-  var quakesUrl = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson';
-  var $quakesList = $('#quakes-list');
-  var map;
+    var quakesUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson';
+    var $quakesList = $('#quakes-list');
+    var map;
+
+    function getTitles(data) {
+        return $.map(data.features, function (value, index) {
+            return {"title": value.properties.title};
+        });
+    }
+
+    function renderTitle(titles) {
+        var source = "<li class='quake-title'> {{title}} </li>";
+        var template = Handlebars.compile(source);
+        var html = "";
+        var i;
+
+        for (i = 0; i < titles.length; i++) {
+            html += template(titles[i]);
+        }
+        html = "<ul>" + html + "</ul>";
+        $quakesList.html(html);
+    }
+
+    $.getJSON(quakesUrl, function (data) {
+        renderTitle(getTitles(data));
+    });
 
 });
