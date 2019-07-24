b4a28fd98ccd20ad31cb144c2ff3d9be85e11510
duke <dukeluo@outlook.com>
2019-07-24 18:09:13 +0800

    feat: complete exercise 04

--- reactjs_koans/exercises/04-Quiz.jsx
+++ reactjs_koans/exercises/04-Quiz.jsx
@@ -2,33 +2,33 @@
 
 var Answers = {
   // Question #1: What is the name of the class that we extend to create components class?
-  // Tip: See any of the exercises
-  answer1: "Enter your answer here",
+  // Tip: See any of the exercises.
+  answer1: "Component",
 
   // Question #2: JSX is converted directly into JavaScript. True or false?
   // Tip: See the first exercise.
-  answer2: null, // true/false?
+  answer2: true, // true/false?
 
   // Question #3: What's the name of the method that must be defined in every component?
-  answer3: "FILL ME IN",
+  answer3: "render",
 
   // Question #4: If I want to set `div` component's HTML class, what attribute
   //              should I declare in JSX?
   // Tip: See exercise #2.
-  answer4: "class",
+  answer4: "className",
 
   // Question #5: A component's properties can be changed after its initialization. True or false?
-  // Tip: See exercise #3
-  answer5: null, // true/false?
+  // Tip: See exercise #3.
+  answer5: false, // true/false?
 
   // Question #6: What's the name of the method for changing the component's state?
-  // Tip: See exercise #3
-  answer6: 'stateSet',
+  // Tip: See exercise #3.
+  answer6: "setState",
 
   // Question #7: You must bind a component's methods (except `render`)
   //              to make it possible to change the state. True or false?
-  // Tip: See exercise #3
-  answer7: null // true/false?
+  // Tip: See exercise #3.
+  answer7: true // true/false?
 };
 
 export default Answers;
