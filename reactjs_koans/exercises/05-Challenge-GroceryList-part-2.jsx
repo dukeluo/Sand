9b691244d1bf07a74c30290deaf218896d4d8215
duke <dukeluo@outlook.com>
2019-07-25 22:06:21 +0800

    feat: complete part 2 of exercise 05

--- reactjs_koans/exercises/05-Challenge-GroceryList-part-2.jsx
+++ reactjs_koans/exercises/05-Challenge-GroceryList-part-2.jsx
@@ -43,7 +43,15 @@ class GroceryList extends React.Component {
   // Hint #2: Remember about the case where input is empty.
   // Hint #3: Name of the new grocery item will be stored in `this.state.newGroceryName`.
   addGroceryItem() {
-    // Put your code here
+    let groceryName = this.state.newGroceryName;
+
+    if (groceryName.length !== 0) {
+      this.setState({
+        groceries: this.state.groceries.concat({
+          name: groceryName
+        })
+      });
+    }
   }
 
   render() {
@@ -62,13 +70,15 @@ class GroceryList extends React.Component {
     // Here are components for task #2.
     newProductInput = <input className='new-item' type="text" onChange={this.inputChanged}/>;
     // Something is missing here... Will anything happen if you click this button now?
-    newProductAddButton = <button className='add-product'>Add new Product</button>;
+    newProductAddButton = <button className='add-product' onClick={this.addGroceryItem}>Add new Product</button>;
 
     return (
       <div>
         <ul>
           {groceriesComponents}
         </ul>
+        {newProductInput}
+        {newProductAddButton}
       </div>
     );
   }
